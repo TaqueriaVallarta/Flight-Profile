4f8177c996ff1cbbee5e3fcc91fa5dac3f96ba29
Andrew Lindner <lindnera1661@gmail.com>
2024-09-09 15:20:35 -0500

    Added some initial functions
    Signed-off-by: Andrew Lindner <lindnera1661@gmail.com>

--- MainLoop.py
+++ MainLoop.py
@@ -1,1 +1,72 @@
-import numpy
+from math import pi, exp, sqrt
+
+
+def base_cross_area(body_diameter, fin_thickness, fin_height):
+    return (pi * (body_diameter / 2) ** 2) + 3 * fin_height * fin_thickness
+
+
+# Drag Force, takes in what you would expect
+def drag_force(cross_area, air_density, drag_coef, velocity):
+    return cross_area * air_density * drag_coef * (velocity ** 2)
+
+
+# Self-Explanatory
+def inches_to_meters(inches):
+    return (inches * 2.54) / 100
+
+
+# Takes in the input/initial values and has methods to calculate other stuff
+class Atmosphere:
+    def __init__(self, temp_0, p_0, h_0):
+        self.temp_0 = temp_0  # Initial temperature in Kelvin
+        self.p_0 = p_0  # Initial pressure in Pascals
+        self.h_0 = h_0  # Initial altitude in meters
+        self.M = 0.0289652  # Molar Mass of Dry Air, kg/mol
+        self.R = 8.3144626  # Universal Gas Constant J/K*mol
+        self.g = 9.81  # Gravitational acceleration, m/s^2
+        self.L = 0.0065  # Temperature lapse rate, K/m
+        self.T0 = 288.15  # Standard temperature, K
+        self.U = 11000  # Tropopause height, m
+        self.H_tp = 6500  # measured in meters (got from wikipedia)
+        self.gamma = 1.4  # measured for air at 0° C, TODO: find any differing values
+
+    def temperature(self, h):
+        """Calculate the temperature at a given altitude h."""
+        if h >= self.U:
+            return self.temp_0 - self.L * (self.U - self.h_0)
+        else:
+            return self.temp_0 - self.L * (h - self.h_0)
+
+    def pressure(self, h):
+        """Calculate the pressure at a given altitude h."""
+        temp = self.temperature(h)
+        if h >= self.U:
+            return (self.p_0 * (1 - (temp / self.temp_0)) ** ((self.g * self.M) / (self.L * self.R))
+                    * exp(-(h - self.U) / self.H_tp))
+        else:
+            return self.p_0 * (1 - (temp / self.temp_0)) ** ((self.g * self.M) / (self.L * self.R))
+
+    def density(self, h):
+        """Calculate the air density at a given altitude h."""
+        temp = self.temperature(h)
+        pressure = self.pressure(h)
+        return pressure / ((self.R / self.M) * temp)
+
+    def speed_of_sound(self, h):
+        return sqrt(1.4 * self.R * self.temperature(h) / self.M)
+
+
+# in future maybe make this a class, with every all ready to go for simulation
+def main():
+    pressure_0 = 101125
+    temperature_0 = 288.15
+    height_0 = 0
+    atmosphere = Atmosphere(temperature_0, pressure_0, height_0)
+
+    diameter = inches_to_meters(8)
+    fin_thickness = inches_to_meters(.5)
+    fin_height = inches_to_meters(12)
+
+    cross_area_rkt = base_cross_area(diameter, fin_thickness, fin_height)
+
+
