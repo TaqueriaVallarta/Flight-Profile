c71e8a7329489e446138afdbf10a32e9902fe70e
Andrew Lindner <101734846+TaqueriaVallarta@users.noreply.github.com>
2024-09-09 20:37:44 -0500

    Added TODOs, moved rk4 to its own file, added functions to rocket class
    Signed-off-by: Andrew Lindner <101734846+TaqueriaVallarta@users.noreply.github.com>

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import DragSetup
 from DragSetup import DragSetup
 from Motor import Motor
+from rk4 import rk4_step
 
 
 # Self-Explanatory
@@ -29,9 +30,16 @@ class Rocket:
         force = drag + thrust + weight
         return force / self.mass(time)
 
+    def rkt_rk4_step(self, time, dt):
+        self.height_asl, self.velocity = rk4_step(self.height_asl, self.velocity, self.acceleration, dt, time)
+
+    def update_agl(self):
+        self.height_agl = self.height_agl - self.initial_height_asl
+
 
 # initializes all the values
 def initialize():
+    # todo: make initialize values somewhat realistic
     fin_thickness = inches_to_meters(.5)
     fin_height = inches_to_meters(12)
     drag_coef = .5
@@ -48,44 +56,6 @@ def initialize():
     return Rocket(drag_setup, motor, dry_mass)
 
 
-# rk4 program, takes in the values and acceleration as a function
-def rk4_step(height, velocity, acceleration, dt, t):
-    """
-    Perform a single step of the RK4 method.
-
-    Args:
-    height: float - Initial position.
-    velocity: float - Initial velocity.
-    acceleration: function - Function to compute acceleration a(height, velocity, t).
-    dt: float - Time step size.
-    t: float - Current time.
-
-    Returns:
-    tuple - (new position, new velocity)
-    """
-    # k1 values for position and velocity
-    k1x = velocity
-    k1v = acceleration(height, velocity, t)
-
-    # k2 values for position and velocity
-    k2x = velocity + 0.5 * dt * k1v
-    k2v = acceleration(height + 0.5 * dt * k1x, velocity + 0.5 * dt * k1v, t + 0.5 * dt)
-
-    # k3 values for position and velocity
-    k3x = velocity + 0.5 * dt * k2v
-    k3v = acceleration(height + 0.5 * dt * k2x, velocity + 0.5 * dt * k2v, t + 0.5 * dt)
-
-    # k4 values for position and velocity
-    k4x = velocity + dt * k3v
-    k4v = acceleration(height + dt * k3x, velocity + dt * k3v, t + dt)
-
-    # Compute new position and velocity
-    new_x = height + (dt / 6) * (k1x + 2 * k2x + 2 * k3x + k4x)
-    new_v = velocity + (dt / 6) * (k1v + 2 * k2v + 2 * k3v + k4v)
-
-    return new_x, new_v
-
-
 if __name__ == '__main__':
     print("Height", ",", "Velocity", ",",
           "Acceleration", ",",
@@ -95,10 +65,15 @@ if __name__ == '__main__':
     current_time = 0
     time_step = .1
     while rocket.height_agl >= 0:
-        rocket.height_asl, rocket.velocity = rk4_step(rocket.height_asl, rocket.velocity,
-                                                      rocket.acceleration, time_step, current_time)
-        rocket.height_agl = rocket.height_asl - rocket.initial_height_asl
+        # Update height + velocity using the rk4
+        rocket.rkt_rk4_step(current_time, time_step)
+        # Convert asl to agl
+        rocket.update_agl()
 
+        # output the values with space between them
+        # TODO: setup csv file export using pandas (ask ChatGPT about it)
+        # numpy is good for making the arrays
+        # TODO: decide between MatPlotLib and Sheets/Excel for presentation of data
         print(rocket.height_agl, ",", rocket.velocity, ",",
               rocket.acceleration(rocket.height_agl, rocket.velocity, current_time), ",",
               current_time, ",", rocket.mass(current_time), ",",
