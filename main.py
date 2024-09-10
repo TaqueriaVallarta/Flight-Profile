b5ec283ca1deffe53fdbd8e82734c6baa6c0aa72
TaqueriaVallarta <andrewlindner07@icloud.com>
2024-09-10 18:09:02 -0500

    Fixing output issues (agl -> msl), and parachute values
    Signed-off-by: TaqueriaVallarta <andrewlindner07@icloud.com>

--- main.py
+++ main.py
@@ -13,13 +13,13 @@ def inches_to_meters(inches):
 
 class Rocket:
     def __init__(self, drag_setup: DragSetup, motor: Motor, dry_mass, time=0, dt=.1,
-                 initial_height_asl=0.0):
+                 initial_height_msl=0.0):
         self.drag_setup = drag_setup  # drag setup class
         self.motor = motor  # motor class
         self.dry_mass = dry_mass  # dry mass in kg
-        self.initial_height_asl = initial_height_asl  # Initial Height in m ASL
+        self.initial_height_asl = initial_height_msl  # Initial Height in m ASL
         self.height_agl = 0  # height above ground level
-        self.height_asl = initial_height_asl  # height above ground level
+        self.height_msl = initial_height_msl  # height above ground level
         self.velocity = 0
         self.gravity = self.drag_setup.atmosphere.g
         self.time = time
@@ -29,10 +29,10 @@ class Rocket:
         return self.dry_mass + self.motor.mass(time)
 
     def acceleration(self, height_asl, velocity, time):
-        drag = self.drag_setup.calculate_drag_force(velocity, height_asl, self.dt)
+        drag = self.drag_setup.calculate_drag_force(velocity, height_asl, self.time)
         thrust = self.motor.thrust(time)
         weight = -self.gravity * self.mass(time)
-        if self.height_asl < self.initial_height_asl:
+        if self.height_msl < self.initial_height_asl:
             normal_force = -weight
         else:
             normal_force = 0
@@ -41,25 +41,26 @@ class Rocket:
 
     # Update self using the rk4 function
     def rkt_rk4_step(self):
-        self.height_asl, self.velocity = rk4_step(self.height_asl, self.velocity, self.acceleration, self.dt, self.time)
+        self.height_msl, self.velocity = rk4_step(self.height_msl, self.velocity, self.acceleration, self.dt, self.time)
 
     def update_agl(self):
-        self.height_agl = self.height_asl - self.initial_height_asl
+        self.height_agl = self.height_msl - self.initial_height_asl
 
     def mach(self):
-        return abs(self.velocity / self.drag_setup.atmosphere.speed_of_sound(self.height_asl))
+        return abs(self.velocity / self.drag_setup.atmosphere.speed_of_sound(self.height_msl))
 
     def outputs(self):
-        return [self.time, self.height_agl, self.height_asl, self.velocity,
-                self.acceleration(rocket.height_agl, self.velocity, self.time),
+        return [self.time, self.height_agl, self.height_msl, self.velocity,
+                self.acceleration(self.height_msl, self.velocity, self.time),
                 self.mass(self.time), self.motor.thrust(self.time),
-                self.drag_setup.calculate_drag_force(self.velocity, self.height_agl, self.dt),
-                self.drag_setup.atmosphere.density(self.height_agl),
-                self.drag_setup.atmosphere.pressure(self.height_agl),
-                self.drag_setup.atmosphere.temperature(self.height_agl),
-                self.drag_setup.atmosphere.speed_of_sound(self.height_asl),
+                self.drag_setup.calculate_drag_force(self.velocity, self.height_msl, self.time),
+                self.drag_setup.atmosphere.density(self.height_msl),
+                self.drag_setup.atmosphere.pressure(self.height_msl),
+                self.drag_setup.atmosphere.temperature(self.height_msl),
+                self.drag_setup.atmosphere.speed_of_sound(self.height_msl),
                 self.drag_setup.drag_coef,
-                self.mach()]
+                self.mach(),
+                self.drag_setup.cross_area]
 
     def sim_to_apogee(self):
         while self.velocity >= 0:
@@ -76,18 +77,18 @@ def initialize():
     fin_height = inches_to_meters(12)
     drag_coef = .5
 
-    cross_area_main = pi*(3/2)**2 # area in m^2
-    drag_coef_main = 2
-    inflation_time_main = 5 #time in seconds
-    deployment_altitude = 600 # meters
-    main_parachute = Parachute(cross_area_main, drag_coef_main, inflation_time_main, deployment_altitude=deployment_altitude)
-
-    cross_area_reefed = pi*(3/2)**2 # area in m^2
+    cross_area_reefed = pi * (.5 / 2) ** 2  # area in m^2
     drag_coef_reefed = 2
-    inflation__time_reefed = 5 #time in seconds
+    inflation__time_reefed = 5  # time in seconds
     reefed_parachute = Parachute(cross_area_reefed, drag_coef_reefed, inflation__time_reefed)
 
-
+    cross_area_main = pi * (3 / 2) ** 2  # area in m^2
+    drag_coef_main = 2
+    inflation_time_main = 5  # time in seconds
+    deployment_altitude = 600  # meters
+    main_parachute = Parachute(cross_area_main, drag_coef_main, inflation_time_main,
+                               deployment_altitude=deployment_altitude)
+    main_parachute.start_surface_area = cross_area_reefed
 
     # Initializes with 8" body tube
     drag_setup = DragSetup(fin_thickness, fin_height, drag_coef, reefed_parachute, main_parachute)
@@ -101,8 +102,8 @@ def initialize():
     motor = Motor(wet_mass_motor, burn_time, mean_thrust)
 
     dry_mass_rocket = 1 / 2 * wet_mass_motor  # Kilograms
-    initial_height_asl = drag_setup.atmosphere.h_0
-    return Rocket(drag_setup, motor, dry_mass_rocket, initial_height_asl=initial_height_asl)
+    initial_height_msl = drag_setup.atmosphere.h_0
+    return Rocket(drag_setup, motor, dry_mass_rocket, initial_height_msl=initial_height_msl)
 
 
 if __name__ == '__main__':
@@ -121,7 +122,7 @@ if __name__ == '__main__':
 
         simulation_data.append(rocket.outputs())
 
-        # print(rocket.height_agl, rocket.time)
+        print(rocket.height_agl, rocket.time, rocket.velocity)
         # output the values with space between them
         # Done: setup csv file export using pandas (ask ChatGPT about it)
         # numpy is good for making the arrays
@@ -137,5 +138,6 @@ if __name__ == '__main__':
     Simulation_dataframe = pd.DataFrame(simulation_data,
                                         columns=['Time Stamp', 'Height_agl', 'Height_asl', "Velocity", 'Acceleration',
                                                  'Mass', 'Thrust', 'Drag', 'Air_Density', 'Air_pressure',
-                                                 'Air_Temperature', 'Speed_of_sound', 'Drag_Coefficient', 'Mach'])
+                                                 'Air_Temperature', 'Speed_of_sound', 'Drag_Coefficient',
+                                                 'Mach', 'Crossectional Area'])
     Simulation_dataframe.to_csv("Simulation_data.csv")
